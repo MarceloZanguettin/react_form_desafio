a538f320ac1d49abab0fd1908b49cfeeb9092baa
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-08 08:49:38 -0300

    Criado no Radio as possiveis respostas das perguntas

--- src/Form/Radio.jsx
+++ src/Form/Radio.jsx
@@ -4,6 +4,15 @@ const Radio = ({ pergunta, options, id }) => {
   return (
     <fieldset>
       <legend>{pergunta}</legend>
+      {options.map((option) => (
+        <label
+          key={option}
+          style={{ marginBottom: '1rem', fontFamily: 'monospace' }}
+        >
+          <input type="radio" value={option} />
+          {option}
+        </label>
+      ))}
     </fieldset>
   );
 };
